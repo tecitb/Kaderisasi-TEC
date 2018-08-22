562b3420f414b324db8736d3a14b86063caaae91
Terry Djony <terrydjony@gmail.com>
2018-08-22 22:15:56 +0700

    added pagination for /users

--- src/routes/admin_only.php
+++ src/routes/admin_only.php
@@ -26,6 +26,12 @@ $app->get('/users', function(Request $request, Response $response, array $args) 
     return $response->withJson($error);
   }
 
+  $page = $request->getQueryParam("page");
+  $number_per_items = $request->getQueryParam("items_per_page") ?? 5;
+  if (isset($page)) {
+    $sql .= " LIMIT " . $number_per_items . " OFFSET " . $number_per_items* ($page - 1);
+  }
+
   try {
     $db = $this->get('db');
     $stmt = $db->query($sql);
